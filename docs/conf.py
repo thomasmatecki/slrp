76f608a06c091c9fe36202a6cfdce3dca2442b90
thomas <thomas.matecki@gmail.com>
2020-02-01 19:42:10 -0500

    reorder deps

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 
-import slrp.combos
-import slrp.expressions
+# import slrp.combos
+# import slrp.expressions
 
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(".."))
